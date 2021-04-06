fdb135ab5f52fb1d5b5425e79f012c31f0113cd3
Diego Pacheco <diego.pacheco020603@gmail.com>
2021-04-06 07:37:19 -0600

    Empezando con el Home

---
+++ src/containers/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../assets/styles/Home.scss'
+
+const Home = () => {
+    return(
+        <>
+            <section className="dashboard--contaier">
+                <h1>hola</h1>
+            </section>
+
+            <div id="ciruclo">
+            </div>
+            <div id="ciruclo">
+            </div>
+        </>
+    );
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//REACT REDUX
+
+//ROUTES
+import App from './routes/App';
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+)
